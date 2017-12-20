5a16d6374372fbe1dd88099f27c66ac5cfa06d6d
yz190598886 <190598886@qq.com>
2017-12-20 16:28:48 +0800

    Signed-off-by: yz190598886 <190598886@qq.com>

---
+++ eeee/src/test/java/eeee/AppTest.java
@@ -0,0 +1,25 @@
+package eeee;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+public class AppTest
+{
+
+
+    @BeforeClass
+    public void AppTest( )
+    {
+        System.out.println("00000000000000000");
+    }
+
+    @Test
+    public void one()
+    {
+        System.out.println("111111111111111");
+    }
+
+    @Test
+    public void two()
+    {
+        System.out.println("2222222222222222");
+    }
+}
